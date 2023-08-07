82fe408e47cf515bb3cfdc7ced50a6b3f6007d5c
Jasper Sands <jasper@mindtrip.ai>
2023-08-07 15:19:50 -0700

    secondary

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-// Import the required modules
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
-const port = 3000; // You can choose any available port number
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+const uri = 'mongodb://localhost:27017/SubletifyDB';
+const client = new MongoClient(uri);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB!');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
 
 // Define a sample route
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-// Start the server
+// Call the connectDB function to establish the connection
+connectDB();
+
+// Start the server after the database connection is established
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
